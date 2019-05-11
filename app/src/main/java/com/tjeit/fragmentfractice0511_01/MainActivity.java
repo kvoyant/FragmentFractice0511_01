b0426dbbcf35e40ef2cd8fd4ccbc347b0c53a7ea
yhkim <kvoyant@gmail.com>
2019-05-11 11:43:24 +0900

    액티비티에서 프래그먼트 교체 - 아직 버그 수정중

--- app/src/main/java/com/tjeit/fragmentfractice0511_01/MainActivity.java
+++ app/src/main/java/com/tjeit/fragmentfractice0511_01/MainActivity.java
@@ -2,8 +2,13 @@ package com.tjeit.fragmentfractice0511_01;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
 import com.tjeit.fragmentfractice0511_01.databinding.ActivityMainBinding;
+import com.tjeit.fragmentfractice0511_01.fragments.FragmentTwo;
 
 public class MainActivity extends BaseActivity {
     ActivityMainBinding act;
@@ -18,7 +23,17 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        act.changeFragTwoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fr = new FragmentTwo();
 
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragOne, fr);
+                fragmentTransaction.commit();
+            }
+        });
     }
 
     @Override
