cdb8206f41980141e28df89f8d415eb9a2d3bc4a
yhkim <kvoyant@gmail.com>
2019-05-11 11:09:04 +0900

    fragment의 데이터 바인딩 예시

--- app/src/main/java/com/tjeit/fragmentfractice0511_01/fragments/FragmentOne.java
+++ app/src/main/java/com/tjeit/fragmentfractice0511_01/fragments/FragmentOne.java
@@ -1,5 +1,6 @@
 package com.tjeit.fragmentfractice0511_01.fragments;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.tjeit.fragmentfractice0511_01.R;
+import com.tjeit.fragmentfractice0511_01.databinding.FragmentOneBinding;
 
 public class FragmentOne extends Fragment {
+
+    FragmentOneBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+//        return inflater.inflate(R.layout.fragment_one, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_one, container, false);
+
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        TextView myTextView = getActivity().findViewById(R.id.myTextView);
+//        myTextView.setText("JAVA");
     }
 }
